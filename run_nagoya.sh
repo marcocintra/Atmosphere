c29bd6c70810ddf828a5832b23e5eed9f654868c
Marco Antônio Cintra <marcoantoniocintra@gmail.com>
2025-08-15 19:44:21 -0300

    Update run_nagoya.sh
    Small adjustments

--- run_nagoya.sh
+++ run_nagoya.sh
@@ -6,13 +6,7 @@ INTERP_DIR=output/
 DATASETS=(
 'TF1_Nagoya_TEC_maps_0800'
 'TF1_Nagoya_TEC_maps_1600'
-'TF1_Nagoya_TEC_maps_2000_2200_0000_0200_0400'
-#'TF2_Nagoya_TEC_maps_0800'
-#'TF2_Nagoya_TEC_maps_1600'
-#'TF2_Nagoya_TEC_maps_2000_2200_0000_0200_0400'
-'TF3_Nagoya_TEC_maps_0800_30m'
-'TF3_Nagoya_TEC_maps_1600_30m'
-'TF3_Nagoya_TEC_maps_2000_0400_30m'
+'TF1_Nagoya_TEC_maps_2000_0400'
 )
 
 for dataset in "${DATASETS[@]}"
